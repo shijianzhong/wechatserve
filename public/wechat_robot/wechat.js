0fd48ec58318e850fee9d5f10830a74a5c26368c
jianzhong.shi <jianzhong.shi@vcg.com>
2018-05-30 14:35:05 +0800

    database

--- public/wechat_robot/wechat.js
+++ public/wechat_robot/wechat.js
@@ -11,16 +11,40 @@ const router = require('koa-router')()
 var request = require('request');
 
 router.prefix('/wechat')
+const one = ['车寻人', '车找人', '找人', '寻人']
+const two = ['人寻车', '人找车', '找车', '找人']
+var msgMap = new Map();
 
 
+router.get('/msglist', function (ctx, next) {
+
+    request('https://api.it120.cc/360mall/json/list', (error, response, body)=> {
+        if (!error && response.statusCode == 200) {
+            let resul =JSON.parse(body);
+            if(resul.code==0){
+                resul.data.forEach(item=>{
+                    msgMap.set(resul.jsonData.author,resul.id)
+                })
+            }
+            ctx.body={
+                requestdata:body
+            }
+        }
+    })
+})
 // 登录二维码
 bot.on('scan', (url, code) => {
         if (!/201|200/.test(String(code))) {
-            console.log(`请扫描二维码完成登录: `)
+            // console.log(`请扫描二维码完成登录: `)
             const loginUrl = url.replace(/\/qrcode\//, '/l/')
-            request('http://www.baidu.com', function (error, response, body) {
+            request('https://api.it120.cc/360mall/json/list', function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    console.log(body) // Show the HTML for the baidu homepage.
+                    let resul =JSON.parse(body);
+                    if(resul.code==0){
+                        resul.data.forEach(item=>{
+                            msgMap.set(resul.jsonData.author,resul.id)
+                        })
+                    }
                 }
             })
             router.get('/loginul', function (ctx, next) {
@@ -28,7 +52,6 @@ bot.on('scan', (url, code) => {
                     loginUrl: loginUrl
                 }
             })
-            // QrcodeTerminal.generate(loginUrl, { small: true })
         }
     })
     // 登录成功
@@ -38,18 +61,40 @@ bot.on('scan', (url, code) => {
     })
     // 自动回复
     .on('message', async m => {
-        // await m.say('hello world')
-        // console.log('Receive msg: ' + m.content())
         const contact = m.from() //发送人
         const content = m.content() //内容
-        const room = m.room() //群
+        const room = m.room() //群  room.topic()
         if (room) {
-            if (content.indexOf('车寻人') > -1) {
-                for (let i = 0; i < ctxs.length; i++) {
-                    var res = `【${room.topic()}】^【${contact.name()}】^ ${content}`
-                    ctxs[i].websocket.send(res);
+            var contet = null;
+            one.forEach(item => {
+                if (content.indexOf(item) > -1) {
+                    contet = {
+                        type: 1,
+                        author: contact.name(),
+                        msg: content
+                    }
                 }
-                messagelist.set(contact.name(), res);
+            })
+            two.forEach(item => {
+                if (content.indexOf(item) > -1) {
+                    contet = {
+                        type: 2,
+                        author: contact.name(),
+                        msg: content
+                    }
+                }
+            })
+            if (contet) {
+                let postdata = {content:JSON.stringify(contet)};
+                if(msgMap.has(contact.name())){
+                   postdata['id']=msgMap.get(contact.name());
+                }
+                request.post({
+                    url: 'https://api.it120.cc/360mall/json/set',
+                    formData:postdata
+                }, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {}
+                })
             }
         } else {
             if (contact.name() != "Sking") {}
