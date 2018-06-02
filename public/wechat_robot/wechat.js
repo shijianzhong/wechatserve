cd497329556b71223785307734ef41da24856336
shijianzhong <994129509@qq.com>
2018-06-02 23:08:47 +0800

    keep one

--- public/wechat_robot/wechat.js
+++ public/wechat_robot/wechat.js
@@ -11,13 +11,30 @@ const router = require('koa-router')()
 var request = require('request');
 
 router.prefix('/wechat')
-const one = ['车寻人', '车找人', '找人', '寻人']
-const two = ['人寻车', '人找车', '找车', '找人']
+const one = ['车寻人', '车找人', '找人', '寻人', '满人', '满车', '车满', '人满']
+const two = ['人寻车', '人找车', '找车', '寻车']
 var msgMap = new Map();
 
 
 router.get('/msglist', function(ctx, next) {
 
+    request('https://api.it120.cc/360mall/json/list', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            let resul = JSON.parse(body);
+            if (resul.code == 0) {
+                resul.data.forEach(item => {
+                    msgMap.set(item.jsonData.author, item.id)
+                })
+            }
+            ctx.body = {
+                requestdata: body
+            }
+        }
+    })
+})
+
+function getInfoList() {
+    return new Promise((resolve, reject) => {
         request('https://api.it120.cc/360mall/json/list', (error, response, body) => {
             if (!error && response.statusCode == 200) {
                 let resul = JSON.parse(body);
@@ -26,16 +43,15 @@ router.get('/msglist', function(ctx, next) {
                         msgMap.set(item.jsonData.author, item.id)
                     })
                 }
-                ctx.body = {
-                    requestdata: body
-                }
+                resolve();
             }
         })
     })
-    // 登录二维码
+}
+// 登录二维码
 bot.on('scan', (url, code) => {
         if (!/201|200/.test(String(code))) {
-            // console.log(`请扫描二维码完成登录: `)
+            console.log(`请扫描二维码完成登录: `)
             const loginUrl = url.replace(/\/qrcode\//, '/l/')
             request('https://api.it120.cc/360mall/json/list', function(error, response, body) {
                 if (!error && response.statusCode == 200) {
@@ -64,11 +80,16 @@ bot.on('scan', (url, code) => {
     })
     // 自动回复
     .on('message', async m => {
+
+
+
         const contact = m.from() //发送人
         const content = m.content() //内容
         const room = m.room() //群  room.topic()
         const tels = content.match(/((((13[0-9])|(15[^4])|(18[0,1,2,3,5-9])|(17[0-8])|(147))\d{8})|((\d3,4|\d{3,4}-|\s)?\d{7,14}))?/g)
         const tel = tels.filter((x) => { if (x) { return x } })
+
+
         if (room) {
             var contet = null;
             one.forEach(item => {
@@ -89,20 +110,22 @@ bot.on('scan', (url, code) => {
                         author: contact.name(),
                         wxid: contact.id,
                         msg: content.replace(/(<img.*?)>/gi, ''),
-                        tel: tel
+                        tel: tel[0]
                     }
                 }
             })
             if (contet) {
                 let postdata = { content: JSON.stringify(contet) };
-                if (msgMap.has(contact.name())) {
-                    postdata['id'] = msgMap.get(contact.name());
-                }
-                request.post({
-                    url: 'https://api.it120.cc/360mall/json/set',
-                    formData: postdata
-                }, function(error, response, body) {
-                    if (!error && response.statusCode == 200) {}
+                getInfoList().then(() => {
+                    if (msgMap.has(contact.name())) {
+                        postdata['id'] = msgMap.get(contact.name());
+                    }
+                    request.post({
+                        url: 'https://api.it120.cc/360mall/json/set',
+                        formData: postdata
+                    }, function(error, response, body) {
+                        if (!error && response.statusCode == 200) {}
+                    })
                 })
             }
         } else {
