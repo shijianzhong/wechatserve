213b0b11cd4bcaa27cb85d6cf1a8385fafe9645a
shijianzhong <994129509@qq.com>
2018-06-03 22:38:32 +0800

    add robot

--- public/wechat_robot/wechat.js
+++ public/wechat_robot/wechat.js
@@ -48,24 +48,36 @@ function getInfoList() {
         })
     })
 }
+
+function sendMsg(text, desp) {
+    request.post({
+        url: 'https://sc.ftqq.com/SCU27446T6848a340eb15bbceea156eef0cbb3c0c5b13e4e00adb0.send',
+        formData: {
+            text: text,
+            desp: desp
+        }
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {}
+    })
+}
 // 登录二维码
 bot.on('scan', (url, code) => {
         if (!/201|200/.test(String(code))) {
             console.log(`请扫描二维码完成登录: `)
             const loginUrl = url.replace(/\/qrcode\//, '/l/')
-            request('https://api.it120.cc/360mall/json/list', function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    let resul = JSON.parse(body);
-                    if (resul.code == 0) {
-                        resul.data.forEach(item => {
-                            msgMap.set(item.jsonData.author, item.id)
-                                // request('https://api.it120.cc/360mall/json/delete?id=' + item.id, function(error, response, body) {
+                // request('https://api.it120.cc/360mall/json/list', function(error, response, body) {
+                //     if (!error && response.statusCode == 200) {
+                //         let resul = JSON.parse(body);
+                //         if (resul.code == 0) {
+                //             resul.data.forEach(item => {
+                //                 msgMap.set(item.jsonData.author, item.id)
+                //                 request('https://api.it120.cc/360mall/json/delete?id=' + item.id, function(error, response, body) {
 
-                            // })
-                        })
-                    }
-                }
-            })
+            //                 })
+            //             })
+            //         }
+            //     }
+            // })
             router.get('/loginul', function(ctx, next) {
                 ctx.body = {
                     loginUrl: loginUrl
@@ -77,18 +89,27 @@ bot.on('scan', (url, code) => {
     .on('login', user => {
         const me = user
         console.log(`User ${user} logined`)
+        sendMsg("登陆成功", `${user}`)
+    })
+    .on('logout', async m => {
+        console.log('logout')
+        console.log(m);
+        sendMsg("机器人退出了", `${user}`)
+    })
+    .on('error', async m => {
+        sendMsg("机器人报错了", `${m}`)
+    })
+    .on('friend', m => {
+        sendMsg("有人加好友", `${m}`)
     })
     // 自动回复
     .on('message', async m => {
-
-
-
         const contact = m.from() //发送人
         const content = m.content() //内容
         const room = m.room() //群  room.topic()
         const tels = content.match(/((((13[0-9])|(15[^4])|(18[0,1,2,3,5-9])|(17[0-8])|(147))\d{8})|((\d3,4|\d{3,4}-|\s)?\d{7,14}))?/g)
         const tel = tels.filter((x) => { if (x) { return x } })
-
+        console.log(`${contact.name()}-----发送了----：${content}`)
 
         if (room) {
             var contet = null;
@@ -114,7 +135,9 @@ bot.on('scan', (url, code) => {
                     }
                 }
             })
+
             if (contet) {
+                sendMsg(contact.name(), content) //发送日志
                 let postdata = { content: JSON.stringify(contet) };
                 getInfoList().then(() => {
                     if (msgMap.has(contact.name())) {
