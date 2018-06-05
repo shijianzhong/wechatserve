fbdc26727bbc00482ff47abf55b2abe62f516c18
jianzhong.shi <jianzhong.shi@vcg.com>
2018-06-05 10:26:30 +0800

    Extraction of public functions into classes

--- public/wechat_robot/wechat.js
+++ public/wechat_robot/wechat.js
@@ -4,6 +4,7 @@ const {
     Contact,
     MediaMessage
 } = require('wechaty')
+const wechatmethod = require('./wechatmethod')
 const QrcodeTerminal = require('qrcode-terminal')
 const bot = Wechaty.instance()
 
@@ -14,85 +15,19 @@ router.prefix('/wechat')
 const one = ['车寻人', '车找人', '找人', '寻人', '满人', '满车', '车满', '人满']
 const two = ['人寻车', '人找车', '找车', '寻车']
 var msgMap = new Map();
-
-
-router.get('/msglist', function (ctx, next) {
-
-    request('https://api.it120.cc/360mall/json/list', (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            let resul = JSON.parse(body);
-            if (resul.code == 0) {
-                resul.data.forEach(item => {
-                    msgMap.set(item.jsonData.author, item.id)
-                })
-            }
-            ctx.body = {
-                requestdata: body
-            }
-        }
-    })
+router.get('/msglist', async function (ctx, next) {  //获取信息列表
+    let result =await wechatmethod.getMsgList();
+    ctx.body =result;
 })
 router.get('/delate', (ctx, next) => {
-    request('https://api.it120.cc/360mall/json/list', function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            let resul = JSON.parse(body);
-            if (resul.code == 0) {
-                resul.data.forEach(item => {
-                    msgMap.set(item.jsonData.author, item.id)
-                    request('https://api.it120.cc/360mall/json/delete?id=' + item.id, function(error, response, body) {
 
-                    })
-                })
-            }
-        }
-    })
 })
 
-function getInfoList() {
-    return new Promise((resolve, reject) => {
-        request('https://api.it120.cc/360mall/json/list', (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                let resul = JSON.parse(body);
-                if (resul.code == 0) {
-                    resul.data.forEach(item => {
-                        msgMap.set(item.jsonData.author, item.id)
-                    })
-                }
-                resolve();
-            }
-        })
-    })
-}
-
-function sendMsg(text, desp) {
-    request.post({
-        url: 'https://sc.ftqq.com/SCU27446T6848a340eb15bbceea156eef0cbb3c0c5b13e4e00adb0.send',
-        formData: {
-            text: text,
-            desp: desp
-        }
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {}
-    })
-}
 // 登录二维码
 bot.on('scan', (url, code) => {
         if (!/201|200/.test(String(code))) {
             console.log(`请扫描二维码完成登录: `)
             const loginUrl = url.replace(/\/qrcode\//, '/l/')
-            // request('https://api.it120.cc/360mall/json/list', function(error, response, body) {
-            //     if (!error && response.statusCode == 200) {
-            //         let resul = JSON.parse(body);
-            //         if (resul.code == 0) {
-            //             resul.data.forEach(item => {
-            //                 msgMap.set(item.jsonData.author, item.id)
-            //                 request('https://api.it120.cc/360mall/json/delete?id=' + item.id, function(error, response, body) {
-
-            //                 })
-            //             })
-            //         }
-            //     }
-            // })
             router.get('/loginul', function (ctx, next) {
                 ctx.body = {
                     loginUrl: loginUrl
@@ -104,18 +39,18 @@ bot.on('scan', (url, code) => {
     .on('login', user => {
         const me = user
         console.log(`User ${user} logined`)
-        sendMsg("登陆成功", `${user}`)
+        wechatmethod.sendMsg("登陆成功", `${user}`)
     })
     .on('logout', async m => {
         console.log('logout')
         console.log(m);
-        sendMsg("机器人退出了", `${user}`)
+        wechatmethod.sendMsg("机器人退出了", `${user}`)
     })
     .on('error', async m => {
-        sendMsg("机器人报错了", `${m}`)
+        wechatmethod.sendMsg("机器人报错了", `${m}`)
     })
     .on('friend', m => {
-        sendMsg("有人加好友", `${m}`)
+        wechatmethod.sendMsg("有人加好友", `${m}`)
     })
     // 自动回复
     .on('message', async m => {
@@ -128,8 +63,6 @@ bot.on('scan', (url, code) => {
                 return x
             }
         })
-        console.log(`${contact.name()}-----发送了----：${content}`)
-
         if (room) {
             var contet = null;
             one.forEach(item => {
@@ -154,23 +87,16 @@ bot.on('scan', (url, code) => {
                     }
                 }
             })
-
             if (contet) {
-                sendMsg(contact.name(), content) //发送日志
+                wechatmethod.sendMsg(contact.name(), content) //发送日志
                 let postdata = {
                     content: JSON.stringify(contet)
                 };
-                getInfoList().then(() => {
-                    if (msgMap.has(contact.name())) {
-                        postdata['id'] = msgMap.get(contact.name());
-                    }
-                    request.post({
-                        url: 'https://api.it120.cc/360mall/json/set',
-                        formData: postdata
-                    }, function (error, response, body) {
-                        if (!error && response.statusCode == 200) {}
-                    })
-                })
+                msgMap = await wechatmethod.getInfoList();
+                if (msgMap.has(contact.name())) {
+                    postdata['id'] = msgMap.get(contact.name());
+                }
+                wechatmethod.postData(postdata);
             }
         } else {
             if (contact.name() != "Sking") {}
@@ -181,4 +107,3 @@ var wechatapp = {
     router: router
 }
 module.exports = wechatapp;
-// bot.start()
---
+++ public/wechat_robot/wechatmethod.js
@@ -0,0 +1,52 @@
+var request = require('request');
+class WechatMethod {
+    static getMsgList() {
+        return new Promise((resolve, reject) => {
+            request('https://api.it120.cc/360mall/json/list', (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    resolve(body)
+                }
+            })
+        })
+    }
+    static getInfoList() {
+        return new Promise((resolve, reject) => {
+            request('https://api.it120.cc/360mall/json/list', (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    var msgMap = new Map();
+                    let resul = JSON.parse(body);
+                    if (resul.code == 0) {
+                        resul.data.forEach(item => {
+                            msgMap.set(item.jsonData.author, item.id)
+                        })
+                    }
+                    resolve(msgMap);
+                }
+            })
+        })
+    }
+    static deleteInfo(id) { //删除数据
+        request('https://api.it120.cc/360mall/json/delete?id=' + id, function (error, response, body) {
+        })
+    }
+    static postData(postadta) {
+        request.post({
+            url: 'https://api.it120.cc/360mall/json/set',
+            formData: postadta
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {}
+        })
+    }
+    static sendMsg(text, desp) {
+        request.post({
+            url: 'https://sc.ftqq.com/SCU27446T6848a340eb15bbceea156eef0cbb3c0c5b13e4e00adb0.send',
+            formData: {
+                text: text,
+                desp: desp
+            }
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {}
+        })
+    }
+}
+module.exports = WechatMethod;
