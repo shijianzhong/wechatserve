950123c03a5322b48d17f4b449ab036dac54544a
史建忠 <994129509@qq.com>
2019-04-12 14:11:13 +0800

    2019年04月12日14:11:13

--- routes/receivemsg.js
+++ routes/receivemsg.js
@@ -5,7 +5,7 @@ const wechatmethod = require('../public/wechat_robot/wechatmethod.js')
 const padch = require('../public/controller/padchatcontroller')
 const htmlqrcode = require('qrcode')
 const wxgzhmethod = require('../public/wxgzh/publicmethod')
-const padchatapp = require('../public/padchat_robot/padchat')
+// const padchatapp = require('../public/padchat_robot/padchat')
 const crypto = require('crypto')
 
 router.post('/', async (ctx, next) => {
@@ -32,7 +32,7 @@ router.post('/', async (ctx, next) => {
                 let suc = wxgzhmethod.message(result.xml, "发布成功")
                 ctx.res.setHeader('Content-Type', 'application/xml')
                 ctx.res.end(suc)
-                padchatapp.Sns();
+                // padchatapp.Sns();
             } else {
                 var realcontent = await wechatmethod.getMsgListForGXH(result.xml.Content);
                 var as = "";
